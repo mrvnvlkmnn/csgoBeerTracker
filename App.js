bb884fd7df378c8cb240a6a76161bef7a40d88f1
Marvin <marvin.volkmann.mv@gmail.com>
2022-05-14 20:16:14 +0200

    added basic design structure

--- App.js
+++ App.js
@@ -1,10 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import { Icon } from "@rneui/themed";
+
+const players = [
+  { id: 0, name: 'Marvin', kills: 13 },
+  { id: 1, name: 'Nico', kills: 20 },
+  { id: 2, name: 'Philipp', kills: 17 }
+];
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <View style={styles.tableWrapper}>
+        <Text style={styles.sectionTitle}>First place: </Text>
+        <Text style={styles.sectionFirstPlaceTitle}>Nico </Text>
+        <View style={styles.spacer}>
+          <Text style={{ color: 'white', fontSize: 1 }}>spacer</Text>
+        </View>
+        <View style={{ marginHorizontal: 20, marginVertical: 10}}>
+          <FlatList style={{ display: 'flex' }} data={players} renderItem={({ item }) =>
+            <View style={styles.playerTableContainer} key={item.id}>
+              <View style={styles.playerContainer}>
+                <View style={styles.circle}></View>
+                <Text style={styles.player}>{item.name}</Text>
+              </View>
+              <View style={styles.playerContainer}>
+                <View style={styles.circle}></View>
+                <View style={styles.killsWrapper}>
+                  <Text style={styles.player}>{item.kills} Kills</Text>
+                </View>
+              </View>
+            </View>} />
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'center', marginBottom: 10}}>
+          <Text style={{color: 'white', marginRight: 2}}>Overview</Text>
+          <Icon style={[styles.angleIcon, {transform: [{rotate: '180deg'}]}]} name='angle-down' type='font-awesome' color='white'/>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +46,56 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  angleIcon: {
+    bottom: 2.5,
+    marginLeft: 2
+  },
+  tableWrapper: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
+    backgroundColor: '#252427',
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'normal',
+    color: '#FFFFFF',
+  },
+  sectionFirstPlaceTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    paddingLeft: 50,
+    color: '#EEEEEE',
+    marginBottom: 10,
+  },
+  spacer: {
+    width: '100%',
+    backgroundColor: 'white',
+    marginBottom: 10,
+  },
+  playerTableContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  playerContainer: {
+    flexDirection: 'row', 
+    justifyContent: 'flex-start',
+  },
+  killsWrapper: {
+    flexDirection: 'row',
+    justifyContent: 'flex-end',
+    width: 45,
+  },
+  circle: {
+    width: 10, 
+    height: 10, 
+    backgroundColor: 'white', 
+    marginTop: 1.5, 
+    borderRadius: 5, 
+    marginRight: 2,
+  },
+  player: {
+    paddingBottom: 10,
+    color: 'white',
+  }
 });
---
+++ README.md
@@ -0,0 +1,5 @@
+# CSGO Beer Tracker
+
+type 'npm install -g expo-cli' to install expo on your system
+
+next type 'expo start' to start the server
