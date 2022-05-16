071248cc94cf2443686880221ccfe421aa264234
Marvin <marvin.volkmann.mv@gmail.com>
2022-05-16 21:37:34 +0200

    changed kills icon based on rankings

--- App.js
+++ App.js
@@ -3,10 +3,12 @@ import { StyleSheet, Text, View, FlatList } from 'react-native';
 import { Icon } from "@rneui/themed";
 
 const players = [
-  { id: 0, name: 'Marvin', kills: 13 },
-  { id: 1, name: 'Nico', kills: 20 },
-  { id: 2, name: 'Philipp', kills: 17 }
-];
+  { id: 0, name: 'Marvin', kills: 25, place: 1, oldPlace: 2 },
+  { id: 1, name: 'Nico', kills: 17, place: 2, oldPlace: 1 },
+  { id: 2, name: 'Philipp', kills: 13, place: 3, oldPlace: 4},
+  { id: 3, name: 'Christof', kills: 10, place: 4, oldPlace: 3}
+].sort((a,b) => (a.place >b.place) ? 1 : -1);
+
 
 export default function App() {
   return (
@@ -17,7 +19,7 @@ export default function App() {
         <View style={styles.spacer}>
           <Text style={{ color: 'white', fontSize: 1 }}>spacer</Text>
         </View>
-        <View style={{ marginHorizontal: 20, marginVertical: 10}}>
+        <View style={{ marginHorizontal: 20, marginVertical: 10 }}>
           <FlatList style={{ display: 'flex' }} data={players} renderItem={({ item }) =>
             <View style={styles.playerTableContainer} key={item.id}>
               <View style={styles.playerContainer}>
@@ -25,16 +27,28 @@ export default function App() {
                 <Text style={styles.player}>{item.name}</Text>
               </View>
               <View style={styles.playerContainer}>
-                <View style={styles.circle}></View>
+                <Icon style={{top: 2.5}}
+                  size={10}
+                  containerStyle={{width: 10, height: 10}}
+                  name={item.place < item.oldPlace ? 'arrow-up-right' : 'arrow-down-right'}
+                  type='feather'
+                  color='white'
+                />
                 <View style={styles.killsWrapper}>
                   <Text style={styles.player}>{item.kills} Kills</Text>
                 </View>
               </View>
-            </View>} />
+            </View>}
+          />
         </View>
-        <View style={{flexDirection: 'row', justifyContent: 'center', marginBottom: 10}}>
-          <Text style={{color: 'white', marginRight: 2}}>Overview</Text>
-          <Icon style={[styles.angleIcon, {transform: [{rotate: '180deg'}]}]} name='angle-down' type='font-awesome' color='white'/>
+        <View style={{ flexDirection: 'row', justifyContent: 'center', marginBottom: 10 }}>
+          <Text style={{ color: 'white', marginRight: 2 }}>Overviews</Text>
+          <Icon style={[styles.angleIcon, { transform: [{ rotate: '180deg' }] }]}
+            name='angle-down'
+            type='font-awesome'
+            color='white'
+            onPress={() => console.log('test')}
+          />
         </View>
       </View>
       <StatusBar style="auto" />
@@ -78,7 +92,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   playerContainer: {
-    flexDirection: 'row', 
+    flexDirection: 'row',
     justifyContent: 'flex-start',
   },
   killsWrapper: {
@@ -87,11 +101,11 @@ const styles = StyleSheet.create({
     width: 45,
   },
   circle: {
-    width: 10, 
-    height: 10, 
-    backgroundColor: 'white', 
-    marginTop: 1.5, 
-    borderRadius: 5, 
+    width: 10,
+    height: 10,
+    backgroundColor: 'white',
+    marginTop: 1.5,
+    borderRadius: 5,
     marginRight: 2,
   },
   player: {
